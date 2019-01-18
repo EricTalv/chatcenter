de0e1863acef824abced8a22f517c09e1b9bf2fd
EricTalv <eric.talviste@tptlive.ee>
2019-01-18 20:35:04 +0200

    Removed Comments, added Comment file, needs better comments

--- ChatCenterV2/static/index.js
+++ ChatCenterV2/static/index.js
@@ -38,9 +38,7 @@ $(function() {
   //Retrieve Data from server and
   //Display on DOM
   socket.on('chat', function (data) {
-    //$messageArea.append('<li>' + data['name'] + ' ' + data['message'] + '</li>');
-    $messageArea.append($('<li>').text(data['name']));
-    $messageArea.append($('<li>').text(data['message']));
+    $messageArea.append('<li>' + data['name'] + ' ' + data['message'] + '</li>');
   });
 
 
